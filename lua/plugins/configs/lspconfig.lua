7d79ad80c5b2e8616bcb3be74e47f2897c4d5334
Kotone Debbra <kotonede@su-b3.msk.21-school.ru>
2023-01-03 21:57:10 +0300

    feat:clangd lsp opts

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -64,6 +64,66 @@ lspconfig.sumneko_lua.setup {
   },
 }
 
-lspconfig.clangd.setup{}
+lspconfig.clangd.setup{
+  cmd = {
+        'clangd',
+        '--background-index',
+        '--pch-storage=memory',
+        '--suggest-missing-includes',
+        '--clang-tidy',
+        '--fallback-style=google',
+        '-j=4',
+    },
+    extensions = {
+        inlay_hints = {
+            only_current_line = false,
+            only_current_line_autocmd = 'CursorHold',
+            show_parameter_hints = true,
+            parameter_hints_prefix = '<- ',
+            other_hints_prefix = '=> ',
+            max_len_align = false,
+            max_len_align_padding = 1,
+            right_align = false,
+            right_align_padding = 7,
+            highlight = 'Keyword',
+            priority = 100,
+        },
+    },
+    settings = {
+        clangd = {
+            init_options = {
+                clangdFileStatus = true,
+                usePlaceholders = true,
+                completeUnimported = true,
+                semanticHighlighting = true,
+            },
+            filetypes = {
+                'c',
+                'cpp',
+                'objc',
+                'objcpp',
+                'cuda',
+                'cc',
+                'hpp',
+                'h',
+                'cxx',
+            },
+            root_dir = {
+                '.git',
+                '.clangd',
+                '.clang-tidy',
+                '.clang-format',
+                'configure.ac',
+                'build/',
+                'Build/',
+                'CMakeLists.txt',
+            },
+            single_file_support = true,
+            flags = {
+                debounce_text_change = 150,
+            },
+        },
+    },
+}
 
 return M
